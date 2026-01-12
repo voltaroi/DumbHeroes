c94154e030ba618beb654caae24baee2990b9fae
Matis <matisconan2006@gmail.com>
2026-01-12 14:29:30 +0100

    Add pseudo

--- src/client.js
+++ src/client.js
@@ -1,15 +1,24 @@
-import { initNetwork } from "./network";
-import { Game } from "./game";
+//import { initNetwork } from "./network";
+//import { Game } from "./game";
 
 
 const canvas = document.getElementById('gameCanvas');
+const nameInput = document.getElementById('nameInput');
+const loginButton = document.getElementById('loginButton');
+
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
 
+loginButton.addEventListener('click', () => {
+    const name = nameInput.value;
+});
+
 resizeCanvas();
 window.addEventListener('resize', resizeCanvas);
 
-initNetwork(game);
+//initNetwork(game);
+
+
 
